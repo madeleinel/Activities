352f829420823c5f0397d317ccb827fdfed41a83
madeleinel <madeleinelinder@hotmail.com>
2017-07-05 22:13:02 +0100

    Starting to set up the database and first table for the website; Creating a PostgreSQL database named ‘activities’ locally. Installing pg, a PostgreSQL client for Node.js, to help make the database connection. Creating a ‘models’ folder with a containing ‘database.js’ file containing the database code. Creating a table entitled activity_options through the database.js file.

---
+++ models/database.js
@@ -0,0 +1,18 @@
+// Set up the PostgreSQL client for Node.js entitled 'pg'
+var pg = require('pg'),
+    // Set connectionString to be the default url provided by the system(?) or the PostgreSQL port for the relevant database
+    connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/activities',
+    // A client is established to interact with the database
+    client = new pg.Client(connectionString);
+// A connection/communication with the database is established for the client using connect()
+client.connect();
+
+var query = client.query (
+  // For each activity being saved into the database, provide:
+  // - The item id (automatically increasing integer)
+  // - A name of min 1 and max 20 characters
+  // - A time estimation of 1-8 characters, and
+  // - A category nomination of 1-10 characters
+  'CREATE TABLE activity_options(id SERIAL PRIMARY KEY, name VARCHAR(20) not null, time VARCHAR(8) not null, category VARCHAR(10) not null)');
+// The connection between the database and the client is closed via end()
+query.on('end', () => { client.end(); });
